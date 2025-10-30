2c7f834307cd4a3b027559aee192e08589c4642c
FehFialho <fernandaklechowicz@gmail.com>
2025-10-30 11:41:48 -0300

    Start Login Page

--- my-first-native-app/app/index.tsx
+++ my-first-native-app/app/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Button, Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 
 export default function HomeScreen() {
 
@@ -10,24 +10,39 @@ export default function HomeScreen() {
   return (
     <>
       <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
-        <Text style={styles.colorBlue}>Hello World!</Text>
-        <View style={styles.square}></View>
-        <TextInput placeholder="Digite Aqui..." onChangeText={text => setText(text)}></TextInput>
-        <Text style={styles.colorBlue}>{text}</Text>
+      
+        <Text>Welcome!</Text>
+      
+        <TextInput placeholder="Email" style={styles.input}></TextInput>
+        <TextInput placeholder="Password" style={styles.input}></TextInput>
 
-        {/* Type 1 Button */}
         <TouchableOpacity onPress={() => console.log("Button Clicked!")}>
           <View>
-            <Text>Button</Text>
+            <Text style={styles.button}>Login</Text>
           </View>
         </TouchableOpacity>
 
-        {/* Type 2 Button */}
-        <Button title="Press Me" onPress={() => console.log("Second Button Clicked")}></Button>
+        <TouchableOpacity onPress={() => console.log("Button Clicked!")}>
+          <View>
+            <Text style={{color:"red"}}>Forgot Password?</Text>
+          </View>
+        </TouchableOpacity>
 
-        <Image 
-          style={{width: 300, height: 300}}
-          source={require("../assets/images/capao-raso.jpg")}></Image>
+        <View  style={{flexDirection: "row", justifyContent: "space-around", width: 400}}>
+
+            <TouchableOpacity onPress={() => console.log("Button Clicked!")}>
+              <View>
+                <Text style={{color:"red"}}>Google</Text>
+              </View>
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={() => console.log("Button Clicked!")}>
+              <View>
+                <Text style={{color:"blue"}}>FaceBook</Text>
+              </View>
+            </TouchableOpacity>
+
+        </View>
 
       </View>
     </>
@@ -35,6 +50,23 @@ export default function HomeScreen() {
 }
 
 const styles = StyleSheet.create({
+
+  button: {
+    backgroundColor: "red",
+    borderRadius: 15,
+    width: 310,
+    height: 40,
+    padding: 10
+  },
+
+  input:{
+    backgroundColor: "gray",
+    borderRadius: 15,
+    height: 40,
+    width: 310,
+    padding: 10
+  },
+
   colorBlue:{
     color: "blue",
     fontSize: 12
